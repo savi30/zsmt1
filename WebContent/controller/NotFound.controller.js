9000fe317e4520c57715ddab865caf5b80e538e1
savi30 <savianubogdan@gmail.com>
2017-08-18 06:34:58 +0300

    Catch invalid hashes

---
+++ WebContent/controller/NotFound.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"zsmt1/controller/BaseController"
+], function (BaseController) {
+   "use strict";
+   return BaseController.extend("zsmt1.controller.NotFound", {
+      onInit: function () {
+      }
+   });
+});
