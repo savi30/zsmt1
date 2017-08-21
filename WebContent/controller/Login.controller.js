c5dd9c46b7a8ef6f658634093c47b9add4b42566
savi30 <savianubogdan@gmail.com>
2017-08-21 17:32:32 +0300

    Implement functional login for manager

--- WebContent/controller/Login.controller.js
+++ WebContent/controller/Login.controller.js
@@ -11,31 +11,17 @@ sap.ui.define([
    "use strict";
   
    var isManager = false;
-   
+   var username ;
+   var password ;
+   var oRouter ;
    return BaseController.extend("zsmt1.controller.Login", {
 	   
 	   
 	   onInit: function () {
-		   this.getView().setModel(new JSONModel({
-				name : "",
-				email : ""
-			}));
-
-			// attach handlers for validation errors
-			sap.ui.getCore().attachValidationError(function (evt) {
-				var control = evt.getParameter("element");
-				if (control && control.setValueState) {
-					control.setValueState("Error");
-				}
-			});
-			sap.ui.getCore().attachValidationSuccess(function (evt) {
-				var control = evt.getParameter("element");
-				if (control && control.setValueState) {
-					control.setValueState("None");
-				}
-			});
-			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		   oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			oRouter.getRoute("login").attachMatched(this._onRouteMatched, this);
+			
+			
 		},
 		
 		_onRouteMatched : function (oEvent) {
@@ -46,61 +32,20 @@ sap.ui.define([
 		
 		handleLogin : function(oEvent){
 	
-			var view = this.getView();
-			var inputs = [
-				view.byId("nameInput"),
-				view.byId("passwordInput")
-			];
- 
-			jQuery.each(inputs, function (i, input) {
-				if (!input.getValue()) {
-					input.setValueState("Error");
-				}
-			});
- 
-		
-			var canContinue = true;
-			jQuery.each(inputs, function (i, input) {
-				if ("Error" === input.getValueState()) {
-					canContinue = false;
-					return false;
-				}
-			});
- 
-			if (canContinue) {
-				
-				if(isManager){
-					if(inputs[0].getValue() == "{i18n>managerUsername}" && inputs[1].getValue() == "{i18n>managerPassword}"){
-						var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-						oRouter.navTo("manager");
-					}
-				}else{
-					
-				}
-				
-			} else {
-				jQuery.sap.require("sap.m.MessageBox");
-				MessageBox.alert("Complete your input first.");
+			    username = this.getView().byId("nameInput").getValue();
+			    password = this.getView().byId("passwordInput").getValue();
+			 
+			if(username == "Manager" && password == "1234"){
+				oRouter.navTo("manager");
+			}else{
+				MessageBox.alert("Incorrect username or password");
 			}
+
+			username = this.getView().byId("nameInput").setValue("");
+		    password = this.getView().byId("passwordInput").setValue("");
+			
+			
 		},
-		
-		typeEMail : SimpleType.extend("email", {
-			formatValue: function (oValue) {
-				return oValue;
-			},
-			parseValue: function (oValue) {
-				//parsing step takes place before validating step, value can be altered
-				return oValue;
-			},
-			validateValue: function (oValue) {
-				// The following Regex is NOT a completely correct one and only used for demonstration purposes.
-				// RFC 5322 cannot even checked by a Regex and the Regex for RFC 822 is very long and complex.
-				var mailregex = /^\w+[\w-+\.]*\@\w+([-\.]\w+)*\.[a-zA-Z]{2,}$/;
-				if (!oValue.match(mailregex)) {
-					throw new ValidateException("'" + oValue + "' is not a valid email address");
-				}
-			}
-		}),
 		
 		onNavBack: function () {
 			var oHistory = History.getInstance();
