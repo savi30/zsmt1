294f92ec47ecbde34c3548939cf10542ead41704
savi30 <savianubogdan@gmail.com>
2017-08-21 10:50:07 +0300

    Add Employee main screen

---
+++ WebContent/controller/Employee.controller.js
@@ -0,0 +1,49 @@
+sap.ui.define([
+	"jquery.sap.global",
+   "zsmt1/controller/BaseController",
+   "sap/m/MessageToast",
+   "sap/ui/core/routing/History"
+], function (JQuery, BaseController, MessageToast, History) {
+   "use strict";
+  
+   return BaseController.extend("zsmt1.controller.Employee", {
+	   
+	   onInit: function () {
+
+		},
+		
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+				oRouter.navTo("App", {}, true);
+			}
+		},
+			handleOpenToolbar : function (oEvent) {
+			var oButton = oEvent.getSource();
+ 
+			// create action sheet only once
+			if (!this._actionSheet) {
+				this._actionSheet = sap.ui.xmlfragment(
+					"zsmt1.view.EmployeeToolbar",
+					this
+				);
+				this.getView().addDependent(this._actionSheet);
+			}
+ 
+			this._actionSheet.openBy(oButton);
+		},
+		onLogout:function(){
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("App", {}, true);
+		},
+		onEdit:function(){
+			MessageToast.show("Implement Edit");
+		}
+
+	});
+});
