6775c596c2c0f99a268e237a9b9868123790ae53
savi30 <savianubogdan@gmail.com>
2017-08-21 22:55:15 +0300

    Add EmployeeSkill Tab

--- WebContent/controller/Login.controller.js
+++ WebContent/controller/Login.controller.js
@@ -1,63 +1,57 @@
-sap.ui.define([
-	"jquery.sap.global",
-   "zsmt1/controller/BaseController",
-   "sap/m/MessageToast",
-   "sap/ui/core/routing/History",
-	'sap/m/MessageBox',
-	'sap/ui/model/SimpleType',
-	'sap/ui/model/ValidateException',
-	'sap/ui/model/json/JSONModel'
-], function (JQuery, BaseController, MessageToast, History, MessageBox, SimpleType, ValidateException, JSONModel) {
-   "use strict";
-  
-   var isManager = false;
-   var username ;
-   var password ;
-   var oRouter ;
-   return BaseController.extend("zsmt1.controller.Login", {
-	   
-	   
-	   onInit: function () {
-		   oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.getRoute("login").attachMatched(this._onRouteMatched, this);
-			
-			
-		},
-		
-		_onRouteMatched : function (oEvent) {
-			
-			isManager = oEvent.getParameter("arguments").isManager;
-			
-		},
-		
-		handleLogin : function(oEvent){
-	
-			    username = this.getView().byId("nameInput").getValue();
-			    password = this.getView().byId("passwordInput").getValue();
-			 
-			if(username == "Manager" && password == "1234"){
-				oRouter.navTo("manager");
-			}else{
-				MessageBox.alert("Incorrect username or password");
-			}
+sap.ui.define([ "jquery.sap.global", "zsmt1/controller/BaseController",
+		"sap/m/MessageToast", "sap/ui/core/routing/History",
+		'sap/m/MessageBox', 'sap/ui/model/SimpleType',
+		'sap/ui/model/ValidateException', 'sap/ui/model/json/JSONModel' ],
+		function(JQuery, BaseController, MessageToast, History, MessageBox,
+				SimpleType, ValidateException, JSONModel) {
+			"use strict";
 
-			username = this.getView().byId("nameInput").setValue("");
-		    password = this.getView().byId("passwordInput").setValue("");
-			
-			
-		},
-		
-		onNavBack: function () {
-			var oHistory = History.getInstance();
-			var sPreviousHash = oHistory.getPreviousHash();
+			var isManager = false;
+			var username;
+			var password;
+			var oRouter;
+			return BaseController.extend("zsmt1.controller.Login", {
 
-			if (sPreviousHash !== undefined) {
-				window.history.go(-1);
-			} else {
-				var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-				oRouter.navTo("App", {}, true);
-			}
-		}
+				onInit : function() {
+					
+					oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+					oRouter.getRoute("login").attachMatched(this._onRouteMatched, this);
+					username = this.getView().byId("nameInput").setValue("");
+					password = this.getView().byId("passwordInput").setValue("");
 
-	});
-});
+				},
+
+				_onRouteMatched : function(oEvent) {
+
+					isManager = oEvent.getParameter("arguments").isManager;
+
+				},
+
+				handleLogin : function(oEvent) {
+
+					username = this.getView().byId("nameInput").getValue();
+					password = this.getView().byId("passwordInput").getValue();
+					if (isManager == true) {
+						if (username == "Manager" && password == "1234") {
+							oRouter.navTo("manager");
+						} else {
+							MessageBox.alert("Incorrect username or password");
+						}
+					} else {
+						oRouter.navTo("employee",{
+							employeeId:password
+						});
+					}
+
+					username = this.getView().byId("nameInput").setValue("");
+					password = this.getView().byId("passwordInput").setValue("");
+				},
+
+				onNavBack : function() {
+					
+						oRouter.navTo("App", {}, true);
+					
+				}
+
+			});
+		});
