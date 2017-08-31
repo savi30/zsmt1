9eb0094371854119e331123ce0f2cfa45b1461b8
savi30 <savianubogdan@gmail.com>
2017-08-31 08:40:52 +0300

    capacity

--- WebContent/controller/Report.controller.js
+++ WebContent/controller/Report.controller.js
@@ -1,3 +1,4 @@
+
 sap.ui.define([
 	"jquery.sap.global",
    "zsmt1/controller/BaseController",
@@ -5,13 +6,50 @@ sap.ui.define([
    "sap/ui/core/routing/History"
 ], function (JQuery, BaseController, MessageToast, History) {
    "use strict";
-  
+  var oModel;
    return BaseController.extend("zsmt1.controller.Report", {
 	   
 	   onInit: function () {
 
-		},
-		
+	   },
+	   onPress:function(oEvent){
+		   var oRouter = sap.ui.core.UIComponent
+			.getRouterFor(this);
+		   oRouter.navTo("managerEmployee", {
+				employeeId : oEvent.getSource()
+						.getBindingContext()
+						.getProperty(
+								"IdEmployee")
+								});
+	   },
+	   onAfterRendering:function(){
+		   
+		   var oChart = this.getView().byId("capacityChart");
+		   
+		   var cap = oChart.getPercentage();
+		   
+		   if(cap < 30){
+			   oChart.setValueColor("Error");
+		   }else if(cap < 70){
+			   oChart.setValueColor("Critical");
+		   }else{
+			   oChart.setValueColor("Good");
+		   }
+	   },
+	   	onBeforeRendering:function(){
+		   
+		   var oChart = this.getView().byId("capacityChart");
+		   
+		   var cap = oChart.getPercentage();
+		   
+		   if(cap < 30){
+			   oChart.setValueColor("Error");
+		   }else if(cap < 70){
+			   oChart.setValueColor("Critical");
+		   }else{
+			   oChart.setValueColor("Good");
+		   }
+	   }
 
 	});
 });
